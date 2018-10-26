9eaa4de761a4a4c03447630dd4c70a08774a4eb1
Kieran Rimmer <kieranrimmer@gmail.com>
2018-10-27 10:58:42 +1100

    Runtime test: initial test built

--- cpp_sandbox/src/skiplist.cpp
+++ cpp_sandbox/src/skiplist.cpp
@@ -1,7 +1,9 @@
 
 #include <iostream>
+#include <vector>
 
 using std::cout;
+using std::vector;
 
 // For a tree-based index in C++, assuming it is best to fit nodes into cache lines, is it better to: (a) use polymorphism / burn space on a vtable pointer, (b) use some sort of flag to check the node type at runtime, (c) something else?
 
@@ -51,22 +53,49 @@ struct RuntimeCheckNode {
   RuntimeCheckNode *getNext() {
     return isLeafNode() ? nullptr : reinterpret_cast<RuntimeCheckNode*>(data);
   }
+  // RuntimeCheckNode() = default;
+  RuntimeCheckNode(const RuntimeCheckNode &other) = default;
+  RuntimeCheckNode(RuntimeCheckNode &&other) = default;
+  RuntimeCheckNode &operator=(const RuntimeCheckNode &other) = default;
+  RuntimeCheckNode &operator=(RuntimeCheckNode &&other) = default;
+  ~RuntimeCheckNode() { cout << "Destroying!!!\n\n"; }
+  // RuntimeCheckNode(void* _data, char _flags) : data{_data}, flags{_flags} {}
 };
 
 void runtimeTest() {
   int datum = 2252;
-  auto curNode = new RuntimeCheckNode<int*>{ .data = &datum, .flags = IS_LEAF_FLAG };
-  cout << "Added intital node at: " << &curNode << "\n"; 
-  for(int i; i < 10; ++i) {
-    curNode = new RuntimeCheckNode<int*>{ .data = curNode, .flags = '\0' };
+  size_t vecSize = 10;
+  size_t i = 0;
+  size_t j = 0;
+  {
+    vector< RuntimeCheckNode<int*> *> nodeVec;
+    nodeVec.push_back(new RuntimeCheckNode<int*>{ .data = &datum, .flags =  IS_LEAF_FLAG  });
+    cout << "Added intital node at " << nodeVec.size() << ": " << &nodeVec[nodeVec.size() - 1] << "\n"; 
+    
+    
+    while(i < vecSize) {
+      nodeVec.push_back(new RuntimeCheckNode<int*>{.data = nodeVec[nodeVec.size() - 1], .flags = '\0' });
+      cout << "Added node at " << nodeVec.size() << ": " << &nodeVec[nodeVec.size() - 1] << "\n";
+      ++i;
+    }
+    for(auto elem: nodeVec) {
+          std::cout << "entry " << j << " = " << *((int *) elem->data) << "\n";
+          ++j;
+    }
+    //
+    cout << "Time to search Runtime Checkable nodes\n";
+    auto curNode = nodeVec[nodeVec.size() - 1];
+    while (!curNode->isLeafNode()) {
+      cout << "Searching...\n";
+      curNode = curNode->getNext();
+    }
+    cout << "Found datum = " << *curNode->getData() << "\n";
+    for(auto elem: nodeVec) {
+          delete(elem);
+    }
   }
-  //
-  cout << "Time to search Runtime Checkable nodes\n";
-  while (!curNode->isLeafNode()) {
-    cout << "Searching...\n";
-    curNode = curNode->getNext();
-  }
-  cout << "Found datum = " << *curNode->getData() << "\n";
+  cout << "Test completed!!!\n\n";
+
   
 }
 
