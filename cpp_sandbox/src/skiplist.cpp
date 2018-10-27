e516f838a0223a26948aff447203936e57490a21
Kieran Rimmer <kieranrimmer@gmail.com>
2018-10-27 23:41:40 +1100

    Unit Tests Progressing

--- cpp_sandbox/src/skiplist.cpp
+++ cpp_sandbox/src/skiplist.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <ctime>
 
 using std::cout;
 using std::vector;
@@ -17,6 +18,7 @@ struct BaseNode {
   public:
   virtual DataType getData() = 0;
   virtual BaseNode *getNext() = 0;
+  virtual ~BaseNode() = default;
 };
 
 template<typename DataType>
@@ -28,17 +30,19 @@ struct InternalNode: public BaseNode<DataType> {
   BaseNode<DataType> *getNext() override {
     return next;
   }
+  explicit InternalNode(BaseNode<DataType> *_next) : next{_next} {}
 };
 
 template<typename DataType>
 struct LeafNode: public BaseNode<DataType> {
-  DataType *data;
+  DataType data;
   DataType getData() override {
     return data;
   }
   BaseNode<DataType> *getNext() override {
     return nullptr;
   }
+  explicit LeafNode(DataType _data) : data{_data} {}
 };
 
 template<typename DataType>
@@ -59,49 +63,103 @@ struct RuntimeCheckNode {
   RuntimeCheckNode(RuntimeCheckNode &&other) = default;
   RuntimeCheckNode &operator=(const RuntimeCheckNode &other) = default;
   RuntimeCheckNode &operator=(RuntimeCheckNode &&other) = default;
-  ~RuntimeCheckNode() { cout << "Destroying!!!\n\n"; }
+  ~RuntimeCheckNode() { 
+    // cout << "Destroying!!!\n\n"; 
+  }
   // RuntimeCheckNode(void* _data, char _flags) : data{_data}, flags{_flags} {}
 };
 
+template<typename T>
+vector< std::unique_ptr< BaseNode<T> > > generatePolymorphicVec(T datum, size_t vecSize = 10) {
+  vector< std::unique_ptr< BaseNode<T> > > nodeVec;
+  nodeVec.push_back(std::make_unique< LeafNode<T> >( LeafNode<T>(datum) ));
+  size_t i = 0;
+  while(i < vecSize) {
+    nodeVec.push_back(std::make_unique< InternalNode<T> >(InternalNode<T>( nodeVec[nodeVec.size() - 1].get() ) ));
+    // cout << "Added node #" << i << "\n";
+    ++i;
+  }
+  return nodeVec;
+}
 
-
-void runtimeTest() {
+void polymorphicTest(size_t vecSize) {
   int datum = 2252;
-  size_t vecSize = 10;
   size_t i = 0;
   size_t j = 0;
   {
-    vector< std::unique_ptr< RuntimeCheckNode<int*> > > nodeVec;
-    nodeVec.push_back(std::make_unique< RuntimeCheckNode<int*> >(RuntimeCheckNode<int*>{ .data = &datum, .flags =  IS_LEAF_FLAG  }));
-    cout << "Added intital node at " << nodeVec.size() << ": " << &nodeVec[nodeVec.size() - 1] << "\n"; 
-    
-    
-    while(i < vecSize) {
-      nodeVec.push_back(std::make_unique< RuntimeCheckNode<int*> >(RuntimeCheckNode<int*>{.data = nodeVec[nodeVec.size() - 1].get(), .flags = '\0' }));
-      cout << "Added node at " << nodeVec.size() << ": " << &nodeVec[nodeVec.size() - 1] << "\n";
-      ++i;
-    }
-    for(auto &elem: nodeVec) {
-          std::cout << "entry " << j << " = " << *((int *) elem->data) << "\n";
-          ++j;
-    }
+    vector< std::unique_ptr< BaseNode<int*> > > nodeVec = generatePolymorphicVec(&datum, vecSize);
     //
-    cout << "Time to search Runtime Checkable nodes\n";
+    cout << "Time to search Polymorphic nodes\n";
+    // CLOCK SETUP BLOCK
+    std::clock_t start;
+    double duration;
+    start = std::clock();
+    // END CLOCK SETUP BLOCK
     auto curNode = nodeVec[nodeVec.size() - 1].get();
-    while (!curNode->isLeafNode()) {
-      cout << "Searching...\n";
+    while (curNode->getData() ==  nullptr) {
+      //cout << "Searching...\n";
       curNode = curNode->getNext();
     }
-    cout << "Found datum = " << *curNode->getData() << "\n";
+    // cout << "Found datum = " << *curNode->getData() << "\n";
     assert(*curNode->getData() == datum);
+    // CLOCK READ BLOCK
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    // END CLOCK READ BLOCK
+    // CLOCK REPORT BLOCK
+    cout << __func__ << " duration: " << duration << '\n';
+    // END CLOCK REPORT BLOCK
+    cout << "Polymorphic Test passed!!!\n";
+  }
+  cout << "Test completed!!!\n";
+}
+
+template<typename T>
+vector< std::unique_ptr< RuntimeCheckNode<T> > > generateRuntimeVec(T datum, size_t vecSize = 10) {
+  vector< std::unique_ptr< RuntimeCheckNode<T> > > nodeVec;
+  nodeVec.push_back(std::make_unique< RuntimeCheckNode<T> >(RuntimeCheckNode<T>{ .data = datum, .flags =  IS_LEAF_FLAG  }));
+  size_t i = 0;
+  while(i < vecSize) {
+    nodeVec.push_back(std::make_unique< RuntimeCheckNode<int*> >(RuntimeCheckNode<int*>{.data = nodeVec[nodeVec.size() - 1].get(), .flags = '\0' }));
+    // cout << "Added node #" << i << "\n";
+    ++i;
+  }
+  return nodeVec;
+}
+
+
+void runtimeTest(size_t vecSize) {
+  int datum = 2252;
+  size_t i = 0;
+  size_t j = 0;
+  {
+    vector< std::unique_ptr< RuntimeCheckNode<int*> > > nodeVec = generateRuntimeVec(&datum, vecSize);
+    //
+    cout << "Time to search Runtime Checkable nodes\n";
+    // CLOCK SETUP BLOCK
+    std::clock_t start;
+    double duration;
+    start = std::clock();
+    // END CLOCK SETUP BLOCK
+    auto curNode = nodeVec[nodeVec.size() - 1].get();
+    while (!curNode->isLeafNode()) {
+      // cout << "Searching...\n";
+      curNode = curNode->getNext();
+    }
+    // cout << "Found datum = " << *curNode->getData() << "\n";
+    assert(*curNode->getData() == datum);
+    // CLOCK READ BLOCK
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    // END CLOCK READ BLOCK
+    // CLOCK REPORT BLOCK
+    cout << __func__ << " duration: " << duration << '\n';
+    // END CLOCK REPORT BLOCK
     cout << "Test passed!!!\n";
   }
   cout << "Test completed!!!\n";
-
-  
 }
 
 int main() {
-  runtimeTest();
+  runtimeTest(1e3);
+  polymorphicTest(1e3);
 }
 
