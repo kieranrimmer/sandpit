1c9421cf71b4c36b60853e053a552be133ac90e7
Kieran Rimmer <kieranrimmer@gmail.com>
2018-10-28 16:57:40 +1100

    Fun and games with array size

--- cpp_sandbox/src/skiplist.cpp
+++ cpp_sandbox/src/skiplist.cpp
@@ -31,12 +31,14 @@ template<typename KeyType, typename DataType>
 struct InternalNode: public BaseNode<KeyType, DataType> {
   static_assert( sizeof(void *) == sizeof(DataType), "DataType type must be pointer-sized" );
   constexpr static size_t max_keyptr_pairs = (sizeof(KeyType) + sizeof(BaseNode<KeyType, DataType> *)) / (ASSUMED_CACHE_LINE_SIZE - sizeof(BaseNode<KeyType, DataType>));
-  BaseNode<KeyType, DataType> *next;
+  constexpr static size_t keyptr_pairs = max_keyptr_pairs > 0 ? max_keyptr_pairs : 1;
+  static_assert(keyptr_pairs > 0, "Array is large enough");
+  BaseNode<KeyType, DataType>* next[keyptr_pairs];
   DataType getData() override {
     return nullptr;
   }
   BaseNode<KeyType, DataType> *getNext() override {
-    return next;
+    return next[0];
   }
   explicit InternalNode(BaseNode<KeyType, DataType> *_next) : next{_next} {}
 };
@@ -45,9 +47,11 @@ template<typename KeyType, typename DataType>
 struct LeafNode: public BaseNode<KeyType, DataType> {
   static_assert( sizeof(void *) == sizeof(DataType), "DataType type must be pointer-sized" );
   constexpr static size_t max_keyval_pairs = (sizeof(KeyType) + sizeof(DataType)) / (ASSUMED_CACHE_LINE_SIZE - sizeof(BaseNode<KeyType, DataType>));
-  DataType data;
+  constexpr static size_t keyval_pairs = max_keyval_pairs > 0 ? max_keyval_pairs : 1;
+  static_assert(keyval_pairs > 0, "Array is large enough");
+  DataType data[keyval_pairs];
   DataType getData() override {
-    return data;
+    return data[0];
   }
   BaseNode<KeyType, DataType> *getNext() override {
     return nullptr;
@@ -58,16 +62,18 @@ struct LeafNode: public BaseNode<KeyType, DataType> {
 template<typename KeyType, typename DataType>
 struct RuntimeCheckNode {
   constexpr static size_t max_keyval_pairs = (sizeof(KeyType) + sizeof(DataType)) / (ASSUMED_CACHE_LINE_SIZE - sizeof(flags_t));
-  void *data;
+  constexpr static size_t keyval_pairs = max_keyval_pairs > 0 ? max_keyval_pairs : 1;
+  static_assert(keyval_pairs > 0, "Array is large enough");
+  void* data[keyval_pairs];
   flags_t flags;
   bool isLeafNode() const {
     return flags & (flags_t) IS_LEAF_FLAG;
   }
   DataType getData() {
-    return isLeafNode() ? reinterpret_cast<DataType>(data) : nullptr;
+    return isLeafNode() ? reinterpret_cast<DataType>(data[0]) : nullptr;
   }
   RuntimeCheckNode *getNext() {
-    return isLeafNode() ? nullptr : reinterpret_cast<RuntimeCheckNode*>(data);
+    return isLeafNode() ? nullptr : reinterpret_cast<RuntimeCheckNode*>(data[0]);
   }
   // RuntimeCheckNode() = default;
   RuntimeCheckNode(const RuntimeCheckNode &other) = default;
@@ -101,6 +107,8 @@ void polymorphicTest(size_t vecSize) {
     vector< std::unique_ptr< BaseNode<int, int*> > > nodeVec = generatePolymorphicVec<int, int*>(&datum, vecSize);
     //
     cout << "Time to search Polymorphic nodes\n";
+    cout << "InternalNode<int, int*>::max_keyval_pairs = " << InternalNode<int, int*>::max_keyptr_pairs << "\n";
+    cout << "LeafNode<int, int*>::max_keyval_pairs = " << LeafNode<int, int*>::max_keyval_pairs << "\n";
     // CLOCK SETUP BLOCK
     std::clock_t start;
     double duration;
@@ -127,10 +135,10 @@ void polymorphicTest(size_t vecSize) {
 template<typename K, typename T>
 vector< std::unique_ptr< RuntimeCheckNode<K, T> > > generateRuntimeVec(T datum, size_t vecSize = 10) {
   vector< std::unique_ptr< RuntimeCheckNode<K, T> > > nodeVec;
-  nodeVec.push_back(std::make_unique< RuntimeCheckNode<K, T> >(RuntimeCheckNode<K, T>{ .data = datum, .flags =  IS_LEAF_FLAG  }));
+  nodeVec.push_back(std::make_unique< RuntimeCheckNode<K, T> >(RuntimeCheckNode<K, T>{ .data = {datum}, .flags =  IS_LEAF_FLAG  }));
   size_t i = 0;
   while(i < vecSize) {
-    nodeVec.push_back(std::make_unique< RuntimeCheckNode<K, T> >(RuntimeCheckNode<K, T>{.data = nodeVec[nodeVec.size() - 1].get(), .flags = '\0' }));
+    nodeVec.push_back(std::make_unique< RuntimeCheckNode<K, T> >(RuntimeCheckNode<K, T>{.data = {nodeVec[nodeVec.size() - 1].get()}, .flags = '\0' }));
     // cout << "Added node #" << i << "\n";
     ++i;
   }
@@ -144,6 +152,7 @@ void runtimeTest(size_t vecSize) {
   size_t j = 0;
   {
     vector< std::unique_ptr< RuntimeCheckNode<int, int*> > > nodeVec = generateRuntimeVec<int, int*>(&datum, vecSize);
+    cout << "RuntimeCheckNode<int, int*>::max_keyval_pairs = " << RuntimeCheckNode<int, int*>::max_keyval_pairs << "\n";
     //
     cout << "Time to search Runtime Checkable nodes\n";
     // CLOCK SETUP BLOCK
