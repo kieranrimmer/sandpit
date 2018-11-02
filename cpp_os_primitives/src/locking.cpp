53381e30a859271c6ef4407d74e65ca3bcb67a3e
Kieran Rimmer <kieranrimmer@gmail.com>
2018-11-03 10:37:45 +1100

    Builtin version of test

--- cpp_os_primitives/src/locking.cpp
+++ cpp_os_primitives/src/locking.cpp
@@ -9,8 +9,9 @@ using std::cout;
 using std::stringstream;
 using std::string;
 
-
+// Globals for CAS lock primitive testing
 std::atomic_bool simpleLock(false);
+bool cas_bool(false);
 
 void testAndSetTest(int arg) {
   stringstream sThread;
@@ -32,6 +33,26 @@ void testAndSetTest(int arg) {
   simpleLock.store(false);
 }
 
+void builtinTestAndSetTest(int arg) {
+  stringstream sThread;
+  std::this_thread::sleep_for(std::chrono::milliseconds(10 / arg));
+  bool falsey = false;
+  uint64_t wait_count = 0;
+  sThread << "#######\n\nThread with arg = " << arg << " invoked builtinTestAndSetTest() \n";
+  while (!__sync_bool_compare_and_swap(&cas_bool, false, true)) {
+    // falsey = false;
+    ++wait_count;
+  }
+  sThread << "Thread with arg = " << arg << " underwent busy waiting " << wait_count << " times\n";
+
+
+  sThread << "Thread with arg = " << arg << " obtained Test And Set lock!!\n";
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  sThread << "Thread with arg = " << arg << " ready to release Test And Set lock!!!\nShall Exit Now!!!\n\n";
+  cout << sThread.str();
+  cas_bool = false;
+}
+
 
 void lockRaceTestAndSet() {
   std::thread threadOne(testAndSetTest, 1);
@@ -43,6 +64,20 @@ void lockRaceTestAndSet() {
 
 }
 
+void lockRaceBuiltinTestAndSet() {
+  std::thread threadOne(builtinTestAndSetTest, 1);
+  std::thread threadTwo(builtinTestAndSetTest, 2);
+  std::thread threadThree(builtinTestAndSetTest, 55);
+  threadOne.join();
+  threadTwo.join();
+  threadThree.join();
+
+}
+
 int main() {
+
   lockRaceTestAndSet();
+
+  lockRaceBuiltinTestAndSet();
+
 }
