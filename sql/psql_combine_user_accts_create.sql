d4bfcbf082712b66d88499560ee82b0beb9388a3
Kieran Rimmer <kieranrimmer@gmail.com>
2018-11-03 16:53:26 +1100

    bugfix: key

--- sql/psql_combine_user_accts_create.sql
+++ sql/psql_combine_user_accts_create.sql
@@ -1,41 +1,66 @@
-
-CREATE TABLE public.service_a
+WITH shared_addr AS 
 (
-    account_id bigint NOT NULL,
-    email_address character varying(255) COLLATE pg_catalog."default"
+SELECT foo.account_a, foo.account_b, row_number() OVER (ORDER BY foo.account_a) AS shared_id
+FROM (
+SELECT 
+  a.account_id as account_a
+, b.account_id as account_b
+FROM
+service_a a
+JOIN
+service_b b
+ON 
+a.email_address = b.email_address
+GROUP BY a.account_id, b.account_id
+) foo
 )
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-CREATE TABLE public.service_b
+SELECT
+account_id,
+email_address
+FROM
 (
-    account_id bigint NOT NULL,
-    email_address character varying(255) COLLATE pg_catalog."default"
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+SELECT
+'A-' || service_a.account_id::text AS account_id,
+service_a.email_address
+FROM service_a
+LEFT OUTER JOIN 
+shared_addr
+ON
+shared_addr.account_a = service_a.account_id
+WHERE shared_addr.account_b IS NULL
+	
+UNION ALL
 
+SELECT
+'B-' ||service_b.account_id::text,
+service_b.email_address FROM service_b
+LEFT OUTER JOIN 
+shared_addr
+ON
+shared_addr.account_b = service_b.account_id
+WHERE shared_addr.account_a IS NULL
+	
+UNION ALL
+(
+SELECT
+'shared-' || shared_addr.shared_id::text,
+service_b.email_address
+FROM service_b
+JOIN 
+shared_addr
+ON
+shared_addr.account_b = service_b.account_id
 
-INSERT INTO public.service_a(
-	account_id, email_address)
- 	VALUES (1, 'a@foo.com');
-INSERT INTO public.service_a(
-	account_id, email_address) VALUES
-			(1, 'b@foo.com');
-			INSERT INTO public.service_a(
-	account_id, email_address) VALUES
-			(2, 'c@foo.com');
-			;
-			
-INSERT INTO public.service_b(
-	account_id, email_address)
-	VALUES (3, 'a@foo.com');
-INSERT INTO public.service_b(
-	account_id, email_address)	VALUES 		(3, 'a@bar.com');
-INSERT INTO public.service_b(
-	account_id, email_address)	VALUES 		(4, 'd@foo.com');
-			
+UNION
+
+SELECT
+'shared-' || shared_addr.shared_id::text,
+service_a.email_address
+FROM service_a
+JOIN 
+shared_addr
+ON
+shared_addr.account_a = service_a.account_id
+)
+) bar
+;
